082be88db098af62df1384e9893a63103898fff5
Ted Neward <ted@tedneward.com>
2017-12-05 17:03:32 -0800

    ApacheUsergrid settings preserved here in case I ever want to try it again

--- apache-usergrid/bootstrap.sh
+++ apache-usergrid/bootstrap.sh
@@ -1,3 +1,3 @@
-curl -X PUT http://localhost:8080/system/database/setup -u superuser:test
-curl -X PUT http://localhost:8080/system/database/bootstrap -u superuser:test
-curl -X GET http://localhost:8080/system/superuser/setup -u superuser:test
+curl -v -X PUT http://localhost:8080/system/database/setup -u superuser:password
+curl -v -X PUT http://localhost:8080/system/database/bootstrap -u superuser:password
+curl -v -X GET http://localhost:8080/system/superuser/setup -u superuser:password
