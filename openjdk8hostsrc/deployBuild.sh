644609231fddb3f466c42a869fd831feeb11cec9
Ted Neward <ted@tedneward.com>
2018-02-12 00:03:51 -0800

    Putting scripts into hostsrc images instead of directly in Dockerfile

---
+++ openjdk8hostsrc/deployBuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -a build/linux-x86_64-normal-server-$1/images/jdk /opt/openjdk8 && \
+find /opt/openjdk8 -type f -exec chmod a+r {} + && \
+find /opt/openjdk8 -type d -exec chmod a+rx {} +
+
+export PATH=/opt/openjdk8/bin:$PATH
+export JAVA_HOME=/opt/openjdk8
---
+++ openjdk8hostsrc/getSource.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+hg clone http://hg.openjdk.java.net/jdk8u/jdk8u openjdk8
+cd openjdk8
+sh ./get_source.sh
+
